8f05c12fcaef35b369b4bfce534234d595e3d1c9
hacker44-h44 <hacker.ht44@gmail.com>
2016-01-24 18:42:10 +0330

    Create chat.lua

---
+++ plugins/chat.lua
@@ -0,0 +1,59 @@
+local function run(msg)
+if msg.text == "hi" then
+	return "Hello"
+end
+if msg.text == "Hi" then
+	return "Hello honey"
+end
+if msg.text == "Hello" then
+	return "Hi"
+end
+if msg.text == "hello" then
+	return "Hi honey"
+end
+if msg.text == "Salam" then
+	return "Salam aleykom"
+end
+if msg.text == "salam" then
+	return "va aleykol asalam"
+end
+if msg.text == "teleguard" then
+	return "Yes?"
+end
+if msg.text == "teleguard" then
+	return "What?"
+end
+if msg.text == "bot" then
+	return "hum?"
+end
+if msg.text == "Bot" then
+	return "Huuuum?"
+end
+if msg.text == "?" then
+	return "Hum??"
+end
+if msg.text == "hacker44" then
+	return "چیکار داری بابامو"
+end
+if msg.text == "bye" then
+	return "Bye Bye"
+end
+end
+
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+		"^[Hh]i$",
+		"^[Hh]ello$",
+		"^[Hh]acker44$",
+		"^[Bb]ot$",
+		"^[Tt]eleguard$",
+		"^[Bb]ye$",
+		"^?$",
+		"^[Ss]alam$",
+		}, 
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
+}
